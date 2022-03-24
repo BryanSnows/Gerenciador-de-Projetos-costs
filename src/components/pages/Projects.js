5433b5d04e4867e1067dd2072f13e27c6d1a81ef
Bryan Neves <bryan.nevesp@gmail.com>
2022-03-24 11:38:37 -0400

    projectsform

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,3 +1,6 @@
+import styles from './Projects.module.css'
+import LinkButton from '../layouts/LinkButton'
+import Container from '../layouts/Container'
 import { useLocation } from 'react-router-dom'
 import Message from '../layouts/Message'
 
@@ -10,14 +13,17 @@ function Projects(){
         if (location.state) {
             message = location.state.message
         }
-    
-
-
-
+        
     return(
-        <div>
-          <h1>Meus Projetos</h1>
-          {message && <Message type="sucess" msg={message}/>}
+        <div className={styles.project_container}>
+           <div className={styles.title_container}>
+             <h1>Meus Projetos</h1>
+             <LinkButton to="/newproject" text="Criar Projeto"/>
+           </div>
+           {message && <Message type="sucess" msg={message}/>}
+           <Container customClass="start">
+             <p>Projetos...</p>
+           </Container>  
         </div>
         
     )
